aee65ff83401001fba8d3ab361b32eab5f127fdc
Rahamatullah01 <yunusrahamatullah123@gmail.com>
2026-09-01 21:51:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+/*1*/
+const firstName = "Rahamah"
+let currentcity = "Lagos"
+console.log(firstName)
+console.log(currentcity)
+
+/*2*/
+const favoritefood = "Yam and Egg Sauce"
+const cookingtime = 30
+const likespicy = "No"
+console.log(favoritefood, cookingtime, likespicy)
+
+/*3*/
+const greeting = "Hello"
+const user = "Coder"
+console.log(greeting + " " + user);
+
+/*4*/
+let welcomeMessage = "Welcome to Javascript!"
+alert(welcomeMessage)
+
+/*5*/
+let itemcount = "5"
+alert("You have " + itemcount + " " + "items in your cart")
+
+/*6*/
+let counter = 0;
+console.log(counter)
+counter = 10
+alert(counter)
+
+/*7*/
+const item = "Keyboard"
+const price = 49
+
+alert("The" + " " + item + " " + "costs" + " " + "$" + price)
+
+
+/*8*/
+let statusMessage = "System Operational"
+console.log(statusMessage)
+alert(statusMessage)
+
+
+/*9*/
+const price2 = 20
+const shipping = 5
+const total = price2 + shipping
+console.log(total)
+
+
+/*10*/
+const birthYear = 2004;
+let currentYear = 2026;
+
+const age = currentYear - birthYear;
+
+alert("I am " + age + " years old");
+console.log(Age)
---
+++ test.js
@@ -0,0 +1,4 @@
+const birthYear = 2004;
+let currentYear = 2026;
+const age = currentYear - birthYear;
+console.log(Age)
